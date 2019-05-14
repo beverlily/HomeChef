a82ca9d5139ff68e67cad6d427e7b4a076dad56a
Jenna Greenberg <jenna.b.greenberg@gmail.com>
2019-05-14 10:01:51 -0400

    Basic file structure and database connection

---
+++ models/database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database
+{
+//properties
+
+  private static $user= 'root';
+  private static $pass= 'root';
+  private static $db = 'HomeChef';
+  private static $dsn = 'mysql:host=localhost;dbname=HomeChef';
+  private static $dbcon;
+
+  private function __construct()
+  {
+
+  }
+
+  public static function getDb(){
+    if(!isset(self::$dbcon)) {
+      try {
+        self::$dbcon = new PDO(self::$dsn, self::$user, self::$pass);
+        self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e){
+        $msg = $e->getMessage();
+
+      }
+    }
+
+    return self::$dbcon;
+  }
+
+}
