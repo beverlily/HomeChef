e774a5b7d00450a03878a409bb621577bc70d4fc
Beverly Li <beverly.li@live.com>
2019-06-02 04:08:08 -0400

    Adds items to cart

--- models/OrderItem.php
+++ models/OrderItem.php
@@ -5,12 +5,11 @@ class OrderItem
     private $quantity;
 	public function __construct(){
 		$this->db = Database::getDb();
-		$this->quantity=0;
 	}
 
 	public function addOrderItem($order_id, $product_id, $quantity, $price){
 		$sql = 'INSERT INTO products_orders(order_id, product_id, quantity, price)
-			VALUES (:order_id, :product_id, :quantity + "$this->quantity", :price)';
+			VALUES (:order_id, :product_id, :quantity, :price)';
 
 		$pstmt = $this->db->prepare($sql);
 		$pstmt->bindParam(':order_id', $order_id);
